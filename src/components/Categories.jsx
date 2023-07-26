80d057529da62a487c1161fe263ea622fc940201
unknown <valen.herrera2000@gmail.com>
2023-07-25 23:34:01 -0300

    adding banner and clothes placeholders

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,36 @@
+import React from 'react';
+import placeholderImg from '../assets/img/clothes-placeholder.png'; // Import the placeholder image
+
 export const Categories = () => {
     return (
-        <div>
+        <main>
             <h2 id="pants">PANTS</h2>
-            <p>Category content for Pants</p>
+            <img src={placeholderImg} alt="Pant 1" />
+            <img src={placeholderImg} alt="Pant 2" />
+            <img src={placeholderImg} alt="Pant 3" />
+            <img src={placeholderImg} alt="Pant 4" />
+
 
             <h2 id="shirts">SHIRTS</h2>
-            <p>Category content for Shirts</p>
+            <img src={placeholderImg} alt="Shirt 1" />
+            <img src={placeholderImg} alt="Shirt 2" />
+            <img src={placeholderImg} alt="Shirt 3" />
+            <img src={placeholderImg} alt="Shirt 4" />
+
 
             <h2 id="sweaters">SWEATERS</h2>
-            <p>Category content for Sweaters</p>
+            <img src={placeholderImg} alt="Sweater 1" />
+            <img src={placeholderImg} alt="Sweater 2" />
+            <img src={placeholderImg} alt="Sweater 3" />
+            <img src={placeholderImg} alt="Sweater 4" />
 
-            <h2 id="accessories">ACCESORIES</h2>
-            <p>Category content for Accessories</p>
-        </div>
+
+            <h2 id="accessories">ACCESSORIES</h2>
+            <img src={placeholderImg} alt="Accessory 1" />
+            <img src={placeholderImg} alt="Accessory 2" />
+            <img src={placeholderImg} alt="Accessory 3" />
+            <img src={placeholderImg} alt="Accessory 4" />
+
+        </main>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import homeBannerImg from '../assets/img/home-banner.png';
+import categoriesBannerImg from '../assets/img/categories-banner.png';
+
 const Header = () => {
     return (
-        <div>
+        <header>
             <h1>Classya</h1>
-            <img src="../assets/img/home-banner.png" alt="Classya Cover" />
-            <h3>Where elegance meets style</h3>
-            <a href="#navbar" className = "shop-now-link">SHOP NOW</a>
-        </div>
+            <img src={homeBannerImg} alt="Classya Cover" /> 
+            <h4>Where elegance meets style</h4>
+            <a href="#navbar" className="shop-now-link">SHOP NOW</a>
+        </header>
     );
 };
+
 export default Header;
